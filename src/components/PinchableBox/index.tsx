c45490b23218775ea9a0a3e5dfc5915467944798
Carlos Gallo <carlos.gallo@somagrupo.com.br>
2021-07-01 17:36:08 -0300

    feature zooom

---
+++ src/components/PinchableBox/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Animated, Dimensions } from 'react-native'
+import { PinchGestureHandler, State } from 'react-native-gesture-handler'
+
+const screen = Dimensions.get('window')
+
+const PinchableBox = ({ source }) => {
+  const scale = new Animated.Value(1)
+  const onPinchEvent = Animated.event(
+    [
+      {
+        nativeEvent: { scale: scale},
+      }
+    ],
+    {
+      useNativeDriver: true
+    }
+  )
+
+  const onPinchStateChange = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      Animated.spring(scale, {
+        toValue: 1,
+        useNativeDriver: true
+      }).start()
+    }
+  }
+
+  return (
+    <PinchGestureHandler
+      onGestureEvent={onPinchEvent}
+      onHandlerStateChange={onPinchStateChange}
+    >
+      <Animated.Image
+        source={source}
+        style={{
+          width: screen.width,
+          height: 300,
+          transform: [{ scale }], //posso colocar
+          resizeMode: 'contain'
+        }}
+        resizeMode='contain'
+      />
+    </PinchGestureHandler>
+  )
+}
+
+export default PinchableBox
--- src/screens/Hello/index.tsx
+++ src/screens/Hello/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text,Image } from 'react-native';
 import { PinchGestureHandler, State } from 'react-native-gesture-handler'
+import ChickenImage from '../../assets/ChickenImage.png';
 
+import PinchableBox from '../../components/PinchableBox'
 
 import { styles } from './styles';
 
 export default function Hello(){
+
     return(
+      <>
         <View style={styles.container}>
-            Olá
+          <Text style={styles.content}>
+            Behold the power of the
+          </Text>
+          <Text style={styles.boldText}>
+            Mighty Zoom
+          </Text>
+
+          <PinchableBox source={ChickenImage} />
+
+          {/* <Image
+          source={ChickenImage}
+          style={styles.image}
+          >
+
+          </Image> */}
         </View>
+      </>
+
     );
 }
